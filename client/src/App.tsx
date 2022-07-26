5d7cc760f94c52a02cee6b870b436ef0002b7bf8
bettykonjo <84689462+bettykonjo@users.noreply.github.com>
2022-07-26 18:36:08 -0400

    adding notfound page in our component and fix the route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import PostData from './components/PostData';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Routes>
       <Route index element={<Header/>} />
       <Route index element={<PostData />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
