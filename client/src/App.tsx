593a1a610326f601cbf06edee293d8fefc6a0469
bettykonjo <84689462+bettykonjo@users.noreply.github.com>
2022-07-26 18:31:33 -0400

    adding css to may header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 import PostData from './components/PostData';
 
 function App() {
   return (
     <Routes>
+      <Route index element={<Header/>} />
       <Route index element={<PostData />} />
     </Routes>
   );
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const HeaderWraper = styled.div`
+  padding: 60px;
+  text-align:start;
+  background: gray;
+  color: white;
+  font-size: 30px;
+`;
+
 
 function Header() {
   return (
-    <div>
+    <HeaderWraper>
         <h1>Fullstack-React-Blog</h1>
         <ul>
             <li> <a href=' Veiw Post'> Veiw Post</a></li>
             <li> <a href='Add Post'> Add Post</a></li>
         </ul>
-    </div>
+    </HeaderWraper>
   )
 }
 
