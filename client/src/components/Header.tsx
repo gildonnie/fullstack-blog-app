49765f5a15e89a8a1b19a1c28ba677971aa21881
donnie gil <gildonnie864@gmail.com>
2022-07-27 08:40:15 -0700

    added header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,15 +1,41 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const Headers = styled.div`
+
+  display: flex;
+  justify-content: space-between;
+  background-color: gray;
+    ul {
+      list-style: none;
+      display: flex;
+        li{
+          margin-top: .5rem;
+          margin-right: 3rem;
+          font-size: 27px;
+          list-style: none;
+            a{
+              text-decoration: none;
+            }
+        }
+    }
+  
+`;
 
 function Header() {
   return (
-    <div>
-        <h1>Fullstack-React-Blog</h1>
-        <ul>
-            <li> <a href=' Veiw Post'> Veiw Post</a></li>
-            <li> <a href='Add Post'> Add Post</a></li>
-        </ul>
-    </div>
-  )
+    <Headers>
+      <h1>Fullstack-React-Blog</h1>
+      <ul>
+        <li>
+          <a href=" Veiw Post"> Veiw Post</a>
+        </li>
+        <li>
+          <a href="Add Post"> Add Post</a>
+        </li>
+      </ul>
+    </Headers>
+  );
 }
 
-export default Header 
+export default Header;
