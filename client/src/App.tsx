bd86b03e64e2a7e0e535d59da0446273490b4f44
jesusquezada <[]>
2022-07-28 08:57:03 -0700

    update(App, Posts): updated links on Posts to route to PostDetails page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import AddPostPage from './pages/AddPostPage';
+import PostDetails from './pages/PostDetails';
 import NotFound from './pages/NotFound';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/addpost" element={<AddPostPage />} />
+        <Route path="/fullpost/:id" element={<PostDetails />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
