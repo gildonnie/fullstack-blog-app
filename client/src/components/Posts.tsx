bb172f70404785d18c4a46f150f44897c35cad10
donnie gil <gildonnie864@gmail.com>
2022-08-01 11:50:24 -0700

    cleaned up code

--- client/src/components/Posts.tsx
+++ client/src/components/Posts.tsx
@@ -10,18 +10,8 @@ const ArticlePost = styled.article`
   border: 1px solid black;
   margin-bottom: 1rem;
 `;
-// const ReadMore = styled.div`
-//   color: blue;
-//   cursor: pointer;
-// `;
 
 function Posts({ posts }: { posts: Post[] }) {
-  // const [postData, setPostData] = useState<Post[]>([]);
-  // useEffect(() => {
-  //   setPostData(posts);
-  //   console.log(postData);
-  // });
-  // const navigate = useNavigate();
   return (
     <>
       <Header />
@@ -33,10 +23,6 @@ function Posts({ posts }: { posts: Post[] }) {
           <section key={id}>
             <p>{body}</p>
             <Link to={`/fullPost/${id}`} state={{ ...posts }}>Read More...</Link>
-            {/* <ReadMore data-id={id} data-title={title} data-body={body}
-            onClick={() => navigate()}
-            onKeyPress={() => navigate(`/fullPost/${id}`)}
-             role="button" tabIndex={0}>  </ReadMore> */}
           </section>
         </ArticlePost>
       ))}
