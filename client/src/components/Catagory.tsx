fe6e20851e378f7e869f74e6f071de6a86212c06
bettykonjo <84689462+bettykonjo@users.noreply.github.com>
2022-08-03 16:49:06 -0400

    updating catagory component

--- client/src/components/Catagory.tsx
+++ client/src/components/Catagory.tsx
@@ -1,123 +1,78 @@
-// import React, { useState,FormEvent } from 'react';
-// import styled from 'styled-components';
-// import { useDispatch, useSelector, shallowEqual } from 'react-redux';
-
-// const Catagorywrapper = styled.footer`
-//   color: lightblue;
-//   padding: 1rem 0;
-//   margin-right: 100px;
-//   text-align: center;
-//   .CatagoryInput{
-//     width: 100px;
-//   }
-//   button {
-//     margin: 0.25rem 0;
-//     width: 100%;
-//   }
-//   h3{
-//     color:black;
-//   }
-// `;
-// function Catagory() {
-//   const [catagory, setCatagory] = useState('All');
-
-//   const handleChange = (event) => {
-//     setCatagory(e.target.value);
-//   };
-//   return (
-//     <Catagorywrapper>
-//       <h3>VIEW CATEGORY</h3>
-//       <form>
-//         <div className="CatagoryInput">
-//           <select value={catagory} onChange={handleChange}>
-//             <option>All</option>
-//             <option>Privet</option>
-//             <option>Public</option>
-//             <option>Ketten</option>
-//           </select>
-//         </div>
-//         <button type="submit">VIEW CATAGORY POSTS</button>
-//       </form>
-//       <h3>ADD A CATEGORY</h3>
-//       <form>
-//         <div className="CatagoryInput">
-//           <input
-//             type="text"
-//             id="searchTerm"
-//             placeholder="Catagory Name"
-//           />
-//         </div>
-//         <button type="submit">ADD.. CATAGORY</button>
-//       </form>
-//     </Catagorywrapper>
-//   );
-// }
-
-// export default Catagory;
 import React, { ChangeEvent, useState, useEffect } from 'react';
-
 import { useDispatch, useSelector } from 'react-redux';
-import { getCategory, setCategory } from '../../store/exampleSlice';
+import { Catagory, setCatagory} from '../store/catagorySlice';
+ import styled from 'styled-components';
+const Catagorywrapper = styled.div`
+  color: lightblue;
+  padding: 1rem 0;
+  margin-right: 100px;
+  text-align: center;
+  .CatagoryInput{
+    width: 100px;
+  }
+  button {
+    margin: 0.25rem 0;
+    width: 100%;
+  }
+  h3{
+    color:black;
+  }
+`;
 
 function Catagory() {
-  const [categories, setCategories] = useState([]);
-  const [newCategory, setNewCategory] = useState('');
-  const [selectedCategory, setSelectedCategory] = useState('');
+  const [catagories, setCatagories] = useState([]);
+  const [newCatagory, setNewCatagory] = useState('');
+  const [selectedCatagory, setSelectedCatagory] = useState('');
   const dispatch = useDispatch();
-  const category = useSelector(getCategory);
+  const catagory = useSelector(Catagory);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('http://localhost:3000/categories/');
-      const json = await res.json();
-      setCategories(json);
+    const response = async () => {
+      const res = await fetch('http://localhost:3000/post/catagories/');
+      const data = await res.json();
+      setCatagories(data);
     };
-    fetchData();
+    response();
   }, []);
 
-  console.log(category);
-
-  const handleCategory = (e: ChangeEvent<HTMLSelectElement>) => {
-    setSelectedCategory(e.target.value);
+  const handleCatagory = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedCatagory(e.target.value);
   };
 
-  const viewCategoryPosts = () => {
-    dispatch(setCategory(selectedCategory));
+  const viewCatagoryPosts = () => {
+    dispatch(setCatagory(selectedCatagory));
   };
 
   const addNew = () => {
-    // setCategories((curr) => {
-    //   return [...curr, newCategory];
-    // });
-    setNewCategory('');
+    setNewCatagory('');
   };
 
   return (
-    <div>
+    <Catagorywrapper>
       <div>
-        <h1>View By Category</h1>
-        <select onChange={handleCategory}>
+        <h1>View By Catagory</h1>
+        <select onChange={handleCatagory}>
           <option value="">All</option>
-          {categories.map((cat: any) => {
-            return <option key={cat}>{cat}</option>;
+          {catagories.map((kitten: string) => {
+            return <option key={kitten}>{kitten}</option>;
           })}
         </select>
-        <button type="button" onClick={viewCategoryPosts}>
-          View Category Post
+        <button type="button" onClick={viewCatagoryPosts}>
+          View Catagory Post
         </button>
       </div>
       <div>
-        <h1>Add New Category</h1>
+        <h1>Add New Catagory</h1>
         <input
-          type="Category Name"
-          value={newCategory}
-          onChange={(e) => setNewCategory(e.target.value)}
+          type="Catagory Name"
+          value={newCatagory}
+          onChange={(e) => setNewCatagory(e.target.value)}
         />
         <button type="button" onClick={addNew}>
-          Add Category
+          Add Catagory
         </button>
       </div>
-    </div>
+    </Catagorywrapper>
   );
 }
 
