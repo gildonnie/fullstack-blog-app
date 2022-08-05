7d84df3b55313ad7ca3343d5b46c26c1a102455f
jesusquezada <[]>
2022-08-05 05:07:38 -0700

    update(EditPage): added onChange handlers for Title, Content, and Category

--- client/src/pages/EditPage.tsx
+++ client/src/pages/EditPage.tsx
@@ -12,14 +12,35 @@ type PostData = {
 function EditPage() {
   const { id } = useParams();
   const [data, setData] = useState<PostData>();
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [category, setCategory] = useState('');
 
   useEffect(() => {
     axios.get(`http://localhost:5000/blog/posts/${id}`)
       .then((response: AxiosResponse) => {
         console.log(response.data);
         setData(response.data);
+        setTitle(response.data.title);
+        setContent(response.data.content);
+        setCategory(response.data.category);
       });
   }, [id]);
+
+  const changeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const enteredName = event.target.value;
+    setTitle(enteredName);
+  };
+
+  const changeContent = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const enteredName = event.target.value;
+    setContent(enteredName);
+  };
+
+  const changeCategory = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const enteredName = event.target.value;
+    setCategory(enteredName);
+  };
 
   return (
     <>
@@ -30,10 +51,27 @@ function EditPage() {
             <input
               id="Title"
               type="text"
-              value={data && data.title}
+              value={title}
+              onChange={changeTitle}
             />
           </label>
-          <textarea value={data && data.content} />
+          <label htmlFor="Content">
+            Content
+            <textarea
+              id="Content"
+              value={content}
+              onChange={changeContent}
+            />
+          </label>
+          <label htmlFor="Category">
+            Edit
+            <input
+              id="Category"
+              type="text"
+              value={category}
+              onChange={changeCategory}
+            />
+          </label>
         </form>
       </div>
       <button type="submit">Update</button>
