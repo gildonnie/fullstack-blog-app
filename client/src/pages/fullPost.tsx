d6ff88a80b8d7505ff161a3fbedb7230d91ea343
jesusquezada <[]>
2022-08-05 13:45:55 -0700

    refactor(fullPost): refactored useState data on fullPost page

--- client/src/pages/fullPost.tsx
+++ client/src/pages/fullPost.tsx
@@ -1,30 +1,28 @@
 import axios, { AxiosResponse } from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-
-type PostData = {
-  id: number;
-  title: string;
-  content: string;
-  category?: string;
-}
+import { Post } from '../types';
 
 function fullPost() {
   const { id } = useParams();
-  const [data, setData] = useState<PostData>();
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [category, setCategory] = useState('');
 
   useEffect(() => {
-    axios.get(`http://localhost:5000/blog/posts/${id}`)
+    axios.get<Post>(`http://localhost:5000/blog/posts/${id}`)
       .then((response: AxiosResponse) => {
-        setData(response.data);
+        setTitle(response.data.title);
+        setContent(response.data.content);
+        setCategory(response.data.category);
       });
   }, [id]);
 
   return (
     <div>
-      <h1>{data && data.title}</h1>
-      <p>{data && data.content}</p>
-      <p>{ data && data.category }</p>
+      <h1>{title}</h1>
+      <p>{content}</p>
+      <p>{category}</p>
     </div>
   );
 }
