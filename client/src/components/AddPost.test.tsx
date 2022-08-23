8cc4496fb2c917de1cf10fc63efdc908e5d77af3
bettykonjo <84689462+bettykonjo@users.noreply.github.com>
2022-08-23 12:57:08 -0400

    test update

--- client/src/components/AddPost.test.tsx
+++ client/src/components/AddPost.test.tsx
@@ -3,19 +3,6 @@ import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import AddPost from './AddPost';
 
-test('The addpost should iclude two text input fields and a submit button with text "ADD POST" ', () => {
-  render(<BrowserRouter><AddPost /></BrowserRouter>); // react-testing-library
-
-  const inputelement = screen.getByText('Title');
-  expect(inputelement).toBeInTheDocument();
-});
-
-test('should render input element', () => {
-  render(<BrowserRouter><AddPost /></BrowserRouter>); // react-testing-library
-
-  const inputelement = screen.getByText('Title');
-  expect(inputelement).toBeInTheDocument();
-});
 
 test('should be able to type textarea, Add your content here', () => {
   render(<BrowserRouter><AddPost /></BrowserRouter>); // react-testing-library
@@ -45,14 +32,4 @@ test('clicking on the "ADD POST" button  the addpost page should disapper', () =
   expect(buttonarea).toBeInTheDocument();
 });
 
-// lets tray here
 
-test('The addpost should iclude two text input fields and a submit button with text "ADD POST" ', () => {
-  render(<BrowserRouter><AddPost /></BrowserRouter>); // react-testing-library
-
-  const titleinput = screen.getByRole('textbox', { name: 'Title' });
-  expect(titleinput).toBeInTheDocument();
-
-  const Categoryinput = screen.getByRole('textbox', { name: 'Category' });
-  expect(Categoryinput).toBeInTheDocument();
-});
